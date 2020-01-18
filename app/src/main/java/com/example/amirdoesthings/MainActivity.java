facafe02c9f89d1460c8e2cc73252ae689cfa866
AmirAzhar <e0309641@u.nus.edu>
2020-01-18 13:24:42 +0800

    Added basic task list functions: 1) Edit text line 2) Enter Button 3) ListView

--- app/src/main/java/com/example/amirdoesthings/MainActivity.java
+++ app/src/main/java/com/example/amirdoesthings/MainActivity.java
@@ -3,12 +3,63 @@ package com.example.amirdoesthings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    private EditText editText;
+    private Button btn;
+    private ListView tasksList;
+
+    private ArrayList<String> tasks;
+    private ArrayAdapter<String> arrayAdapter; //a tool for android studio to fill in lists
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.addTaskText);
+        btn = findViewById(R.id.addTaskButton);
+        tasksList = findViewById(R.id.taskList);
+
+        tasks = StorageHelper.readData(this);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tasks);
+        tasksList.setAdapter(arrayAdapter);
+
+        btn.setOnClickListener(this);
+        tasksList.setOnItemClickListener(this);
+    }
+
+    //save wtv that is typed into the editText into the Arraylist
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.addTaskButton:
+                String newTask = editText.getText().toString();
+                arrayAdapter.add(newTask);
+                editText.setText(""); // change the edit text to empty after entering
+
+                StorageHelper.writeData(tasks,this);
+
+                Toast.makeText(this, "Successfully added task!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        tasks.remove(position);
+        arrayAdapter.notifyDataSetChanged();
+        Toast.makeText(this, "Task successfully deleted!", Toast.LENGTH_SHORT).show();
     }
 }
